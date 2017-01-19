c0e33ac303a914d2bc942aa0c52097631ff32efc
Matthew Kennedy <mk@gatech.edu>
2017-01-19 03:15:49 -0500

    Fix SPI pinout

--- de0-cv/fpga_efi_de0_cv.v
+++ de0-cv/fpga_efi_de0_cv.v
@@ -22,6 +22,6 @@ module fpga_efi_de0_cv(CLOCK_50, CLOCK2_50, GPIO_1, LEDR, RESET_N);
 					  .vrin(GPIO_1[10]),
 					  .ign_a(GPIO_1[12]), .ign_b(GPIO_1[11]), .ign_c(GPIO_1[14]), .ign_d(GPIO_1[13]),
 					  .inj_a(GPIO_1[31]), .inj_b(GPIO_1[32]),
-					  .synced(GPIO_1[2]),
-					  .sck(GPIO_1[4]), .miso(GPIO_1[6]), .mosi(GPIO_1[7]), .cs(GPIO_1[0]));
+					  .synced(LEDR[8]),
+					  .sck(GPIO_1[4]), .miso(GPIO_1[6]), .mosi(GPIO_1[7]), .cs(GPIO_1[5]));
 endmodule
